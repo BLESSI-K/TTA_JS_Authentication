beae656fc5887bfb653d7ba1e43ae490537fc022
BLESSI-k <ucyeyengogablessing@gmail.com>
2025-01-20 21:52:34 +0200

    chore:adding data set

--- scripts.js
+++ scripts.js
@@ -1,15 +1,37 @@
+const users = [
+    {
+        username:'admin',
+        password: 'admin123',
+        role:'admin'
+    },
+    {
+        username:'editor',
+        password: 'editor123',
+        role:'editor'
+    },
+    {
+        username:'viewer',
+        password: 'viewer123',
+        role:'viewer'
+    }
+]
+
+
+
+
 const loginform = document.querySelector('#loginform')
 
 loginform.addEventListener('submit',function(e){
     e.preventDefault();
 
-    const username = document.querySelector('#userName').value;
-    const password = document.querySelector('#Password').value;
+    const username = document.querySelector('#userName').value.trim();
+    const password = document.querySelector('#Password').value.trim();
     const errorContainer = document.querySelector('#errorMessage');
-
-    if(!username.trim() || !password.trim()){
-        errorContainer.textContent ='Please fill in all fields';
-    }else{
-        errorContainer.textContent = '';
-    }
+  
+    const user
+   // if(!username || !password){
+   //     errorContainer.textContent ='Please fill in all fields';
+   // }else{
+   //     errorContainer.textContent = '';
+    //}
 })
